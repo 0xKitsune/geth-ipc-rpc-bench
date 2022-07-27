019054e95f1544fa5b2ec040eadc23c97897dc76
0xKitsune <0xKitsune@protonmail.com>
2022-07-27 09:27:14 -0400

    added ipc path

--- ipc_rpc_test.go
+++ ipc_rpc_test.go
@@ -12,14 +12,13 @@ import (
 
 func BenchmarkIPC(b *testing.B) {
 
-	ipcPath := ""
+	ipcPath := "~/.ethereum/geth.ipc"
 	client, err := ethclient.Dial(ipcPath)
 	if err != nil {
 		b.Fatal("Error when initializing client", err)
 	}
 
 	targetAddress := common.HexToAddress("0x29F06Dc85D64B9748465735A2C61a524Ffd28BDe")
-
 	for n := 0; n < b.N; n++ {
 		client.BalanceAt(context.Background(), targetAddress, nil)
 	}
